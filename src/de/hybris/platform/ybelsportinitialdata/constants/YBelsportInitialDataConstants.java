3be457978bc3475a35a2165fc09ac3caba754f74
Rodrigo Portela da Costa <rpcosta@gmail.com>
2020-01-13 11:25:28 -0300

    Commit just in case

---
+++ src/de/hybris/platform/ybelsportinitialdata/constants/YBelsportInitialDataConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.platform.ybelsportinitialdata.constants;
+
+/**
+ * Global class for all YBelsportInitialData constants.
+ */
+public final class YBelsportInitialDataConstants extends GeneratedYAcceleratorInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "ybelsportinitialdata";
+
+	private YBelsportInitialDataConstants()
+	{
+		//empty
+	}
+}
---
+++ src/de/hybris/platform/ybelsportinitialdata/setup/InitialDataSystemSetup.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package cl.com.bls.b2c.bld.initialdata.setup;
+
+import cl.com.bls.b2c.bld.initialdata.constants.Boldb2cstoreInitialDataConstants;
+import cl.com.bls.b2c.bld.initialdata.dataimport.impl.CoreDataImportService;
+import cl.com.bls.b2c.bld.initialdata.dataimport.impl.SampleDataImportService;
+import de.hybris.platform.commerceservices.setup.AbstractSystemSetup;
+import de.hybris.platform.commerceservices.setup.data.ImportData;
+import de.hybris.platform.commerceservices.setup.events.CoreDataImportedEvent;
+import de.hybris.platform.commerceservices.setup.events.SampleDataImportedEvent;
+import de.hybris.platform.core.initialization.SystemSetup;
+import de.hybris.platform.core.initialization.SystemSetup.Process;
+import de.hybris.platform.core.initialization.SystemSetupContext;
+import de.hybris.platform.core.initialization.SystemSetupParameter;
+import de.hybris.platform.core.initialization.SystemSetupParameterMethod;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Required;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+@SystemSetup(extension = YBelsportInitialDataConstants.EXTENSIONNAME)
+public class InitialDataSystemSetup extends AbstractSystemSetup
+{
+	@SuppressWarnings("unused")
+	private static final Logger LOG = Logger.getLogger(InitialDataSystemSetup.class);
+
+	public static final String STORE_UID = "boldb2cstore";
+	public static final String CATALOG_PREFIX = "bold";
+
+	private static final String IMPORT_CORE_DATA = "importCoreData";
+	private static final String IMPORT_SAMPLE_DATA = "importSampleData";
+	private static final String ACTIVATE_SOLR_CRON_JOBS = "activateSolrCronJobs";
+
+	private CoreDataImportService coreDataImportService;
+	private SampleDataImportService sampleDataImportService;
+
+	/**
+	 * Generates the Dropdown and Multi-select boxes for the project data import
+	 */
+	@Override
+	@SystemSetupParameterMethod
+	public List<SystemSetupParameter> getInitializationOptions()
+	{
+		final List<SystemSetupParameter> params = new ArrayList<SystemSetupParameter>();
+
+		params.add(createBooleanSystemSetupParameter(IMPORT_CORE_DATA, "Import Core Data", true));
+		params.add(createBooleanSystemSetupParameter(IMPORT_SAMPLE_DATA, "Import Sample Data", true));
+		params.add(createBooleanSystemSetupParameter(ACTIVATE_SOLR_CRON_JOBS, "Activate Solr Cron Jobs", true));
+		// Add more Parameters here as you require
+
+		return params;
+	}
+
+	/**
+	 * Implement this method to create initial objects. This method will be called by system creator during
+	 * initialization and system update. Be sure that this method can be called repeatedly.
+	 *
+	 * @param context
+	 *           the context provides the selected parameters and values
+	 */
+	@SystemSetup(type = SystemSetup.Type.PROJECT, process = Process.ALL)
+	public void createProjectData(final SystemSetupContext context)
+	{
+		final List<ImportData> importData = new ArrayList<ImportData>();
+
+		final ImportData boldImportData = new ImportData();
+		boldImportData.setProductCatalogName(CATALOG_PREFIX);
+		boldImportData.setContentCatalogNames(Arrays.asList(CATALOG_PREFIX));
+		boldImportData.setStoreNames(Arrays.asList(STORE_UID));
+		importData.add(boldImportData);
+
+		getCoreDataImportService().execute(this, context, importData);
+		getEventService().publishEvent(new CoreDataImportedEvent(context, importData));
+
+		getSampleDataImportService().execute(this, context, importData);
+		getEventService().publishEvent(new SampleDataImportedEvent(context, importData));
+	}
+
+	public CoreDataImportService getCoreDataImportService()
+	{
+		return coreDataImportService;
+	}
+
+	@Required
+	public void setCoreDataImportService(final CoreDataImportService coreDataImportService)
+	{
+		this.coreDataImportService = coreDataImportService;
+	}
+
+	public SampleDataImportService getSampleDataImportService()
+	{
+		return sampleDataImportService;
+	}
+
+	@Required
+	public void setSampleDataImportService(final SampleDataImportService sampleDataImportService)
+	{
+		this.sampleDataImportService = sampleDataImportService;
+	}
+}
